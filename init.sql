e8f70802954f3546226b0a24b220077c5b2a5159
Hugo Fonseca <hugo.fonseca@bemobi.com.br>
2018-11-03 00:33:39 -0300

    a

--- init.sql
+++ init.sql
@@ -46,3 +46,27 @@ INSERT INTO person(name, email, rating) VALUES
     'person2@gmail.com',
     9
 );
+
+insert into event(name, emergency, icon)
+values ('Habitação e urbanismo', false, 'localhost:8000/habit_urban.jpg');
+
+insert into event(name, emergency, icon)
+values ('Acessibilidade', false, 'localhost:8000/acessibilidade.jpg');
+
+insert into event(name, emergency, icon)
+values ('Saneamento', false, 'localhost:8000/saneamento.jpg');
+
+insert into event(name, emergency, icon)
+values ('Depredação', false, 'localhost:8000/depredacao.jpg');
+
+insert into event(name, emergency, icon)
+values ('Catástrofe', true, 'localhost:8000/catastrofe.jpg');
+
+insert into event(name, emergency, icon)
+values ('Assalto', true, 'localhost:8000/assalto.jpg');
+
+insert into event(name, emergency, icon)
+values ('Arrastão', true, 'localhost:8000/arrastao.jpg');
+
+insert into event(name, emergency, icon)
+values ('Acidente de Trânsito', true, 'localhost:8000/acidente_transito.jpg');
